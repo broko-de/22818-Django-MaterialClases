f80bdab51c2cb64dd802de7f741f6da010183fbf
Fedex <jose.liquin@bue.edu.ar>
2022-09-15 17:40:21 -0300

    Se sube ejemplo de clases para creacion de paquete distribuible

---
+++ Clase8/paquete_distribuible/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="recursos humanos", 
+    version="0.1",
+    description="Paquete de ejemplo de empleados y nomina",
+    author="Codo a Codo",
+    author_email="cac@bue.edu.ar",
+    url="https://www.buenosaires.gob.ar/educacion/codo-codo",
+    packages=['recursos_humanos','recursos_humanos.personal' ],
+    scripts=[]
+)
